9787d628c44a7ddd4caf69cbfc1903af9d0b59f3
Rumman <rummanfardeen4567@gmail.com>
2023-09-07 10:52:29 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import NavBar from "../Components/NavBar";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+
+    </>
+  );
+}
